f4201f32daf2100ba40e92f12a225c56bf023c87
stephaniebeatrice <beatriceladida@gmail.com>
2023-06-19 22:03:56 +0300

    UI changes

--- App.js
+++ App.js
@@ -1,99 +1,105 @@
-import { StatusBar } from "expo-status-bar";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from 'expo-status-bar'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { useState, useLayoutEffect } from 'react'
+import AllPlaces from './screens/AllPlaces'
+import AddPlace from './screens/AddPlace'
+import IconButton from './components/UI/IconButton'
+import { Colors } from './constants/colors'
+import Map from './screens/Map'
+import { Text } from 'react-native'
+import PlaceDetails from './screens/PlaceDetails'
+import { Login } from './screens/Login'
+import { SignUp } from './screens/Signup'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-import AllPlaces from "./screens/AllPlaces";
-import AddPlace from "./screens/AddPlace";
-import IconButton from "./components/UI/IconButton";
-import { Colors } from "./constants/colors";
-import Map from "./screens/Map";
-import { Text } from "react-native";
-
-import PlaceDetails from "./screens/PlaceDetails";
-import { Login } from "./screens/Login";
-import { SignUp } from "./screens/Signup";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 export default function App() {
-  const [email, setEmail] = useState("");
+	const [email, setEmail] = useState('')
 
-  const retrieveData = async () => {
-    try {
-      const value = await AsyncStorage.getItem("key");
-      setEmail(value);
-      if (value !== null) {
-        console.log("Retrieved data: ", value);
-      } else {
-        console.log("No data found.", value);
-      }
-    } catch (error) {
-      console.log("Error retrieving data: ", error);
-    }
-  };
+	const retrieveData = async () => {
+		try {
+			const value = await AsyncStorage.getItem('key')
+			setEmail(value)
+			if (value !== null) {
+				console.log('Retrieved data: ', value)
+			} else {
+				console.log('No data found.', value)
+			}
+		} catch (error) {
+			console.log('Error retrieving data: ', error)
+		}
+	}
 
-  useLayoutEffect(() => {
-    retrieveData();
-  }, [email]);
+	useLayoutEffect(() => {
+		retrieveData()
+	}, [email])
 
-  return (
-    <>
-      <StatusBar style="dark" />
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: Colors.primary500 },
-            headerTintColor: Colors.gray700,
-            contentStyle: { backgroundColor: Colors.gray700 },
-          }}
-        >
-          {email && (
-            <>
-              <Stack.Screen
-                name="Login"
-                component={Login}
-                options={({ navigation }) => ({
-                  title: "Login",
-                  // headerRight: ({ tintColor }) => <IconButton icon="add" size={24} color={tintColor} onPress={() => navigation.navigate("AddPlace")} />,
-                })}
-              />
-              <Stack.Screen
-                name="Signup"
-                component={SignUp}
-                options={({ navigation }) => ({
-                  title: "Sign up",
-                  // headerRight: ({ tintColor }) => <IconButton icon="add" size={24} color={tintColor} onPress={() => navigation.navigate("AddPlace")} />,
-                })}
-              />
-            </>
-          )}
-          <Stack.Screen
-            name="AllPlaces"
-            component={AllPlaces}
-            options={({ navigation }) => ({
-              title: "APPLES",
-              headerRight: ({ tintColor }) => <IconButton icon="add" size={24} color={tintColor} onPress={() => navigation.navigate("AddPlace")} />,
-            })}
-          />
+	return (
+		<>
+			<StatusBar style="dark" />
+			<NavigationContainer>
+				<Stack.Navigator
+					screenOptions={{
+						headerStyle: { backgroundColor: Colors.primary500 },
+						headerTintColor: Colors.gray700,
+						contentStyle: { backgroundColor: Colors.gray700 }
+					}}
+				>
+					{/* Check if email is available */}
+					{email ? (
+						<>
+							<Stack.Screen
+								name="Login"
+								component={Login}
+								options={({ navigation }) => ({
+									title: 'Login'
+								})}
+							/>
+							<Stack.Screen
+								name="Signup"
+								component={SignUp}
+								options={({ navigation }) => ({
+									title: 'Sign up'
+								})}
+							/>
+						</>
+					) : null}
 
-          <Stack.Screen
-            name="AddPlace"
-            component={AddPlace}
-            options={{
-              title: "Add a new Place",
-            }}
-          />
-          <Stack.Screen name="Map" component={Map} />
-          <Stack.Screen
-            name="PlaceDetails"
-            component={PlaceDetails}
-            options={{
-              title: "Loading Place...",
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
-  );
+					<Stack.Screen
+						name="AllPlaces"
+						component={AllPlaces}
+						options={({ navigation }) => ({
+							title: 'APPLES',
+							headerRight: ({ tintColor }) => (
+								<IconButton
+									icon="add"
+									size={24}
+									color={tintColor}
+									onPress={() => navigation.navigate('AddPlace')}
+								/>
+							)
+						})}
+					/>
+
+					<Stack.Screen
+						name="AddPlace"
+						component={AddPlace}
+						options={{
+							title: 'Add a new Place'
+						}}
+					/>
+					<Stack.Screen name="Map" component={Map} />
+					<Stack.Screen
+						name="PlaceDetails"
+						component={PlaceDetails}
+						options={{
+							title: 'Loading Place...'
+						}}
+					/>
+				</Stack.Navigator>
+			</NavigationContainer>
+		</>
+	)
 }
--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,119 +1,194 @@
-import { useCallback, useState } from "react";
-import { Alert, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { useCallback, useState } from 'react'
+import {
+	Alert,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
+	ImageBackground
+} from 'react-native'
 
-import { Colors } from "../../constants/colors";
-import { Place } from "../../models/place";
-import Button from "../UI/Button";
-import LocationPicker from "./LocationPicker";
+import { Colors } from '../../constants/colors'
+import { Place } from '../../models/place'
+import Button from '../UI/Button'
+import LocationPicker from './LocationPicker'
 
 function PlaceForm({ onCreatePlace }) {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredID, setEnteredID] = useState("");
-  const [enteredBreed, setEnteredBreed] = useState("");
-  const [enteredRow, setEnteredRow] = useState("");
-  const [enteredColumn, setEnteredColumn] = useState("");
-  const [enteredYOP, setEnteredYOP] = useState("");
+	const [enteredTitle, setEnteredTitle] = useState('')
+	const [enteredID, setEnteredID] = useState('')
+	const [enteredBreed, setEnteredBreed] = useState('')
+	const [enteredRow, setEnteredRow] = useState('')
+	const [enteredColumn, setEnteredColumn] = useState('')
+	const [enteredYOP, setEnteredYOP] = useState('')
 
-  const [pickedLocation, setPickedLocation] = useState();
-  const [isFormValid, setIsFormValid] = useState(false);
+	const [pickedLocation, setPickedLocation] = useState()
+	const [isFormValid, setIsFormValid] = useState(false)
 
-  const checkFormValidity = () => {
-    const isIDValid = enteredID?.trim() !== "";
-    const isBreedValid = enteredBreed?.trim() !== "";
-    const isRowValid = enteredRow?.trim() !== "";
-    const isColumnValid = enteredColumn?.trim() !== "";
-    const isYOPValid = enteredYOP?.trim() !== "";
-    const isLocationValid = pickedLocation?.trim() !== "";
-    return isIDValid && isBreedValid && isRowValid && isColumnValid && isYOPValid && isLocationValid;
-  };
+	const checkFormValidity = () => {
+		const isIDValid = enteredID?.trim() !== ''
+		const isBreedValid = enteredBreed?.trim() !== ''
+		const isRowValid = enteredRow?.trim() !== ''
+		const isColumnValid = enteredColumn?.trim() !== ''
+		const isYOPValid = enteredYOP?.trim() !== ''
+		const isLocationValid = pickedLocation?.trim() !== ''
+		return (
+			isIDValid &&
+			isBreedValid &&
+			isRowValid &&
+			isColumnValid &&
+			isYOPValid &&
+			isLocationValid
+		)
+	}
 
-  function changeColumnHandler(enteredText) {
-    setEnteredColumn(enteredText);
-    setIsFormValid(checkFormValidity());
-  }
-  function changeRowHandler(enteredText) {
-    setEnteredRow(enteredText);
-    setIsFormValid(checkFormValidity());
-  }
-  function changeIDHandler(enteredText) {
-    setEnteredID(enteredText);
-    setIsFormValid(checkFormValidity());
-  }
-  function changeBreedHandler(enteredText) {
-    setEnteredBreed(enteredText);
-    setIsFormValid(checkFormValidity());
-  }
-  function changeYOPHandler(enteredText) {
-    setEnteredYOP(enteredText);
-    setIsFormValid(checkFormValidity());
-  }
+	function changeColumnHandler(enteredText) {
+		setEnteredColumn(enteredText)
+		setIsFormValid(checkFormValidity())
+	}
+	function changeRowHandler(enteredText) {
+		setEnteredRow(enteredText)
+		setIsFormValid(checkFormValidity())
+	}
+	function changeIDHandler(enteredText) {
+		setEnteredID(enteredText)
+		setIsFormValid(checkFormValidity())
+	}
+	function changeBreedHandler(enteredText) {
+		setEnteredBreed(enteredText)
+		setIsFormValid(checkFormValidity())
+	}
+	function changeYOPHandler(enteredText) {
+		setEnteredYOP(enteredText)
+		setIsFormValid(checkFormValidity())
+	}
 
-  const pickLocationHandler = useCallback(
-    location => {
-      console.log("===========================LOCATION==========================");
-      console.log(location);
-      if (location) setPickedLocation(location);
-      setIsFormValid(checkFormValidity());
-    },
-    [pickedLocation]
-  );
+	const pickLocationHandler = useCallback(
+		(location) => {
+			console.log(
+				'===========================LOCATION=========================='
+			)
+			console.log(location)
+			if (location) setPickedLocation(location)
+			setIsFormValid(checkFormValidity())
+		},
+		[pickedLocation]
+	)
 
-  function savePlaceHandler() {
-    if (!isFormValid) {
-      Alert.alert("Fill all the Details before submitting");
-      return; // Exit the function if the form is not valid
-    }
-    const placeData = new Place(enteredTitle, enteredID, enteredYOP, enteredBreed, enteredRow, enteredColumn, pickedLocation);
-    onCreatePlace(placeData);
-    console.log("ddf", placeData);
-  }
+	function savePlaceHandler() {
+		if (!isFormValid) {
+			Alert.alert('Fill all the Details before submitting')
+			return // Exit the function if the form is not valid
+		}
+		const placeData = new Place(
+			enteredTitle,
+			enteredID,
+			enteredYOP,
+			enteredBreed,
+			enteredRow,
+			enteredColumn,
+			pickedLocation
+		)
+		onCreatePlace(placeData)
+		console.log('ddf', placeData)
+	}
+	return (
+		<ImageBackground
+			source={require('../../assets/formPage.jpeg')}
+			style={styles.backgroundImage}
+		>
+			<View style={styles.overlay} />
+			<ScrollView style={styles.form}>
+				<View style={styles.container}>
+					<Text style={styles.label}>Apple ID</Text>
+					<TextInput
+						style={styles.input}
+						onChangeText={changeIDHandler}
+						value={enteredID}
+					/>
 
-  return (
-    <ScrollView style={styles.form}>
-      <View>
-        <Text style={styles.label}>Apple ID</Text>
-        <TextInput style={styles.input} onChangeText={changeIDHandler} value={enteredID} />
-        <Text style={styles.label}>YOP</Text>
-        <TextInput style={styles.input} onChangeText={changeYOPHandler} value={enteredYOP} />
-        <Text style={styles.label}>Breed</Text>
-        <TextInput style={styles.input} onChangeText={changeBreedHandler} value={enteredBreed} />
-        <Text style={styles.label}>Row</Text>
-        <TextInput style={styles.input} onChangeText={changeRowHandler} value={enteredRow} />
-        <Text style={styles.label}>Column</Text>
-        <TextInput style={styles.input} onChangeText={changeColumnHandler} value={enteredColumn} />
-      </View>
-      {/* <ImagePicker onTakeImage={takeImageHandler} /> */}
-      <LocationPicker onPickLocation={pickLocationHandler} />
-      <Button onPress={savePlaceHandler} disabled={!isFormValid}>
-        Add Place
-      </Button>
-      <View style={styles.view} />
-    </ScrollView>
-  );
+					<Text style={styles.label}>YOP</Text>
+					<TextInput
+						style={styles.input}
+						onChangeText={changeYOPHandler}
+						value={enteredYOP}
+					/>
+					<Text style={styles.label}>Breed</Text>
+					<TextInput
+						style={styles.input}
+						onChangeText={changeBreedHandler}
+						value={enteredBreed}
+					/>
+					<Text style={styles.label}>Row</Text>
+					<TextInput
+						style={styles.input}
+						onChangeText={changeRowHandler}
+						value={enteredRow}
+					/>
+					<Text style={styles.label}>Column</Text>
+					<TextInput
+						style={styles.input}
+						onChangeText={changeColumnHandler}
+						value={enteredColumn}
+					/>
+				</View>
+				{/* <ImagePicker onTakeImage={takeImageHandler} /> */}
+				<LocationPicker onPickLocation={pickLocationHandler} />
+				<Button onPress={savePlaceHandler} disabled={!isFormValid}>
+					Add Place
+				</Button>
+				<View style={styles.view} />
+			</ScrollView>
+		</ImageBackground>
+	)
 }
 
-export default PlaceForm;
+export default PlaceForm
 
 const styles = StyleSheet.create({
-  form: {
-    flex: 1,
-    padding: 24,
-  },
-  view: {
-    height: 200,
-  },
-  label: {
-    fontWeight: "bold",
-    marginBottom: 4,
-    color: Colors.primary500,
-  },
-  input: {
-    marginVertical: 8,
-    paddingHorizontal: 4,
-    paddingVertical: 8,
-    fontSize: 16,
-    borderBottomColor: Colors.primary700,
-    borderBottomWidth: 2,
-    backgroundColor: Colors.primary100,
-  },
-});
+	form: {
+		flex: 1,
+		padding: 30
+	},
+	backgroundImage: {
+		flex: 1,
+		resizeMode: 'cover'
+	},
+	overlay: {
+		...StyleSheet.absoluteFillObject,
+		backgroundColor: 'rgba(0, 0, 0, 0.5)' // Replace with your desired hue color
+	},
+	view: {
+		height: 200
+	},
+	label: {
+		fontWeight: 'bold',
+		marginBottom: 4,
+		color: Colors.primary500,
+		marginRight: 10
+	},
+	input: {
+		width: '60%',
+		height: 40,
+		borderWidth: 1,
+		borderColor: '#ccc',
+		marginBottom: 12,
+		padding: 10,
+		//flex: 1,
+		// marginVertical: 8,
+		// paddingHorizontal: 4,
+		// paddingVertical: 8,
+		fontSize: 16,
+		// borderBottomColor: Colors.primary700,
+		// borderBottomWidth: 2,
+		//backgroundColor: Colors.primary100,
+		borderRadius: 30
+	},
+	submitButton: {
+		backgroundColor: 'green',
+		borderRadius: 30,
+		marginTop: 12,
+		paddingVertical: 10,
+		alignItems: 'center'
+	}
+})
