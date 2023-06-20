93d7a55b46b7fd5b94b18a8f3ddf36eef41a50b2
stephaniebeatrice <beatriceladida@gmail.com>
2023-06-20 15:37:48 +0300

    Change in Placelist and Placeform

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,53 +1,70 @@
-import { useNavigation } from "@react-navigation/native";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { useNavigation } from '@react-navigation/native'
+import { FlatList, StyleSheet, Text, View, ImageBackground } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
 
-import { Colors } from "../../constants/colors";
-import PlaceItem from "./PlaceItem";
+import { Colors } from '../../constants/colors'
+import PlaceItem from './PlaceItem'
 
 function PlacesList({ places }) {
-  const navigation = useNavigation();
+	const navigation = useNavigation()
 
-  function selectPlaceHandler(id) {
-    navigation.navigate("PlaceDetails", {
-      placeId: id,
-    });
-  }
+	function selectPlaceHandler(id) {
+		navigation.navigate('PlaceDetails', {
+			placeId: id
+		})
+	}
 
-  if (!places || places.length === 0) {
-    return (
-      <View style={styles.fallbackContainer}>
-        <Text style={styles.fallbackText}>
-          No places added yet - start adding some!
-        </Text>
-      </View>
-    );
-  }
+	if (!places || places.length === 0) {
+		return (
+			<ImageBackground
+				source={require('../../assets/welcomePage.jpeg')}
+				style={styles.backgroundImage}
+			>
+				<LinearGradient
+					colors={['rgba(0,0,0,0.5)', 'rgba(0,0,0,0)']}
+					style={styles.overlay}
+				/>
+				<View style={styles.fallbackContainer}>
+					<Text style={styles.fallbackText}>
+						No places added yet - start adding some!
+					</Text>
+				</View>
+			</ImageBackground>
+		)
+	}
 
-  return (
-    <FlatList
-      style={styles.list}
-      data={places}
-      keyExtractor={(item, index) => index.toString()}
-      renderItem={({ item }) => (
-        <PlaceItem place={item} onSelect={selectPlaceHandler} />
-      )}
-    />
-  );
+	return (
+		<FlatList
+			style={styles.list}
+			data={places}
+			keyExtractor={(item, index) => index.toString()}
+			renderItem={({ item }) => (
+				<PlaceItem place={item} onSelect={selectPlaceHandler} />
+			)}
+		/>
+	)
 }
 
-export default PlacesList;
+export default PlacesList
 
 const styles = StyleSheet.create({
-  list: {
-    margin: 24,
-  },
-  fallbackContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  fallbackText: {
-    fontSize: 16,
-    color: Colors.primary200,
-  },
-});
+	list: {
+		margin: 24
+	},
+	backgroundImage: {
+		flex: 1,
+		resizeMode: 'cover'
+	},
+	overlay: {
+		...StyleSheet.absoluteFillObject
+	},
+	fallbackContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	fallbackText: {
+		fontSize: 16,
+		color: '#5bc75d'
+	}
+})
